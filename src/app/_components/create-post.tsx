1225f8e66ea4f87bd9db732310fd60e3cd87a827
ggkmkomega <dhuibetilyes@gmail.com>
2024-06-24 04:00:17 +0100

    Test Deploy

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-import { api } from "~/trpc/react";
-
 export function CreatePost() {
-  const router = useRouter();
   const [name, setName] = useState("");
-
-  const createPost = api.post.create.useMutation({
-    onSuccess: () => {
-      router.refresh();
-      setName("");
-    },
-  });
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
       }}
       className="flex flex-col gap-2"
     >
@@ -34,9 +22,8 @@ export function CreatePost() {
       <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isPending}
       >
-        {createPost.isPending ? "Submitting..." : "Submit"}
+        Submit
       </button>
     </form>
   );
