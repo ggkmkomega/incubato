3047acff3a99f747f5b12564a770e4981b0eb6ce
ggkmkomega <dhuibetilyes@gmail.com>
2024-06-29 18:01:15 +0100

    feat: Call component

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useGetCallById } from "hooks/useGetCallById";
+import React, { useState } from "react";
+import Loeader from "~/components/Loeader";
+import MeetingRoom from "~/components/MeetingRoom";
+import MeetingSetup from "~/components/MeetingSetup";
 
 const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room number #{params.id}</div>;
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState();
+  const { call, isCallLoading } = useGetCallById(params.id);
+  if (!isLoaded || isCallLoading) return <Loeader />;
+  return (
+    <main className="h-screen w-full ">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
